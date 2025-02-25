29249505deee426a9b752de728303a21d56a3146
RubenArias <iyg.2019.2@gmail.com>
2025-02-24 21:49:50 -0600

    Guarda en una lista los nombres

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = []
+
+//Agrega los elementos a la lista onclick
+function agregarAmigo() {
+    let nombreamigo = document.getElementById('amigo').value;
+    
+    listaAmigos.push(nombreamigo);
+    console.log(listaAmigos);
+ 
+    return;
+}
