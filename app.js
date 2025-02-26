204dcd6e7ffad4704ab91e304d777f40f32fb805
RubenArias <iyg.2019.2@gmail.com>
2025-02-25 21:18:25 -0600

    Verifica que haya un nombre valido

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ function asignarTextoElemento(elemento, texto) {
 
 function agregarAmigo() {
     let nombreamigo = document.getElementById('amigo').value;
-    lista_de_Amigos.push(nombreamigo);
-    console.log(lista_de_Amigos);
-    document.getElementById('amigo').value="";
-    document.getElementById('amigo').placeholder = 'agrega otro nombre';
-    const nuevoElemento = document.createElement('li');
-    // Asignar texto al nuevo elemento
-    nuevoElemento.textContent = nombreamigo;
-    document.getElementById('listaAmigos').appendChild(nuevoElemento);
-    return;
+    if (nombreamigo == "") {
+        alert("Ingrese un nombre")
+    }else{     
+        
+        lista_de_Amigos.push(nombreamigo);
+        console.log(lista_de_Amigos);
+        document.getElementById('amigo').value="";
+        document.getElementById('amigo').placeholder = 'agrega otro nombre';
+        const nuevoElemento = document.createElement('li');
+        // Asignar texto al nuevo elemento
+        nuevoElemento.textContent = nombreamigo;
+        document.getElementById('listaAmigos').appendChild(nuevoElemento);
+        return;
+    }
 }
